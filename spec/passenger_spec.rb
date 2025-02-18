bc68eba55d2badbdd37615915d06512c2aa6d983
memcmahon <mcmahon.meganelizabeth@gmail.com>
2025-02-18 10:39:02 -0700

    add test comments

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,9 +1,11 @@
 require './lib/passenger'
 
+# This block helps format the test results
 RSpec.configure do |config|
   config.formatter = :documentation
 end
 
+# Your tests should start here!
 RSpec.describe Passenger do 
     
 end
