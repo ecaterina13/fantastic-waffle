f88b442779d6ab41f755103d1f5e14d79f12ca0b
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-02-19 12:58:35 -0600

    Commit

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,9 +1,26 @@
 class Vehicle
-  attr_reader :year, :make, :model
+  attr_reader :year, :make, :model, :passengers
+  attr_accessor :speed
+
   def initialize(year, make, model)
     @year = year
     @make = make
     @model = model
-    
+    @speed = 0
+    @passengers = []
+  end
+
+  def speeding?
+    @speed > 30
+  end
+
+  def add_passenger(passenger)
+    @passengers << passenger
+  end
+
+  def num_adults
+    @passengers.count do |passenger|
+      passenger.age >= 18
+    end
   end
 end
