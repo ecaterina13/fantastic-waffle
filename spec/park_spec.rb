732113888dbdd46593782bbc0fb73dbb4e6bd2c4
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-02-19 13:01:33 -0600

    Commit

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -1,34 +1,46 @@
 require 'rspec'
 require './lib/passenger'
+require './lib/vehicle'
+require './lib/park'
+require 'pry'
 
-
-RSpec.describe Passenger do
+RSpec.describe Park do
   before(:each) do
+    @park = Park.new("great sand dunes", 30)
+    @vehicle = Vehicle.new("2001", "Honda", "Civic")
     @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    @jude = Passenger.new({"name" => "Jude", "age" => 20})  
     @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
   end
 
-  it 'exists' do
-    expect(@charlie).to be_a(Passenger)
-    expect(@taylor).to be_a(Passenger)
-  end
-  it "has info" do 
-    expect(@charlie.name).to eq("Charlie")
-    expect(@charlie.age).to eq(18)
-    expect(@taylor.name).to eq("Taylor")
-    expect(@taylor.age).to eq(12)
-  end
-  it 'checks if a passenger is an adult' do
-    expect(@charlie.adult?).to eq(true)
-    expect(@taylor.adult?).to eq(false)
+  it "exists" do
+    expect(@park).to be_a(Park)
   end
 
-  
-  it 'determines if passenger is a driver' do
-    expect(@charlie.driver?).to be false
-    
-    @charlie.drive
-
-    expect(@charlie.driver?).to be true
+  it "tracks the vehicles" do
+    @vehicle = Vehicle.new("2001", "Honda", "Civic")
+    @park.add_vehicle(@vehicle)
+    expect(@park.vehicles_park).to eq([@vehicle])
   end
+
+  it "counts the passengers" do
+    @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    @jude = Passenger.new({"name" => "Jude", "age" => 20})  
+    @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+    @park.add_passenger(@charlie)
+    @park.add_passenger(@jude)
+    @park.add_passenger(@taylor)
+
+    expect(@park.passengers_park).to eq([@charlie, @jude, @taylor])
+  end
+   
+#   it "returns the adults" do
+#     @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+#     @jude = Passenger.new({"name" => "Jude", "age" => 20})  
+#     @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+#     expect(@park.adult).to eq([@charlie, @jude])
+#   end 
 end
--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,11 +1,34 @@
+require 'rspec'
 require './lib/passenger'
 
-# This block helps format the test results
-RSpec.configure do |config|
-  config.formatter = :documentation
-end
 
-# Your tests should start here!
-RSpec.describe Passenger do 
+RSpec.describe Passenger do
+  before(:each) do
+    @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+  end
+
+  it 'exists' do
+    expect(@charlie).to be_a(Passenger)
+    expect(@taylor).to be_a(Passenger)
+  end
+  it "has info" do 
+    expect(@charlie.name).to eq("Charlie")
+    expect(@charlie.age).to eq(18)
+    expect(@taylor.name).to eq("Taylor")
+    expect(@taylor.age).to eq(12)
+  end
+  it 'checks if a passenger is an adult' do
+    expect(@charlie.adult?).to eq(true)
+    expect(@taylor.adult?).to eq(false)
+  end
+
+  
+  it 'determines if passenger is a driver' do
+    expect(@charlie.driver?).to be false
     
+    @charlie.drive
+   #come back to write another test for this
+    expect(@charlie.driver?).to be true
+  end
 end
